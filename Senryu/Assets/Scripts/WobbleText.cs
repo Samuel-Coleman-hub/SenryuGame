332d32dbf25214561fab6baed7fa9578088a679e
Samuel Coleman <colemanrsamuel@outlook.com>
2021-09-15 22:35:57 +0100

    Added a low poly person & Colour palette

---
+++ Senryu/Assets/Scripts/WobbleText.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WobbleText : MonoBehaviour
+{
+    public TMP_Text textComponent;
+
+    [SerializeField] float wiggleSpeed = 2f;
+    [SerializeField] float wiggleHeight = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        textComponent.ForceMeshUpdate();
+        var textInfo = textComponent.textInfo;
+
+        for(int i = 0; i < textInfo.characterCount; i++)
+        {
+            var charInfo = textInfo.characterInfo[i];
+
+            if (!charInfo.isVisible)
+            {
+                continue;
+            }
+
+            var verts = textInfo.meshInfo[charInfo.materialReferenceIndex].vertices;
+
+            for( int j = 0; j<4; j++)
+            {
+                var orig = verts[charInfo.vertexIndex + j];
+                verts[charInfo.vertexIndex + j] = orig + new Vector3(0, Mathf.Sin(Time.time*wiggleSpeed + orig.x*0.01f) * wiggleHeight, 0);
+            }
+        }
+
+        for(int i = 0; i < textInfo.meshInfo.Length; i++)
+        {
+            var meshInfo = textInfo.meshInfo[i];
+            meshInfo.mesh.vertices = meshInfo.vertices;
+            textComponent.UpdateGeometry(meshInfo.mesh, i);
+        }
+
+    }
+}
