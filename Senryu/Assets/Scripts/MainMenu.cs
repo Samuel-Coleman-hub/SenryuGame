c391eb325f61d720bfe20d90c7a09ad1da03c3a3
Samuel Coleman <colemanrsamuel@outlook.com>
2021-09-19 17:42:23 +0100

    Added UI Transition

--- Senryu/Assets/Scripts/MainMenu.cs
+++ Senryu/Assets/Scripts/MainMenu.cs
@@ -5,13 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transitionAnim;
+
     public void PlayGame()
     {
+        StartCoroutine(LoadScene());
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    //public void ExitGame()
-    //{
-    //    Application.Quit();
-    //}
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+    }
 }
